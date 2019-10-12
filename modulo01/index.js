27d4f853297897f10704aa22741a8d2e2ec2856e
Rogério <roger.lista@gmail.com>
2019-10-12 11:58:39 -0300

    NodeJS módulo 01 - rotas com CRUD de user

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,24 +2,49 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
 // query params = /users?nome=Juca
 // route params = /users/42
 // body params = { nome='Juca', email='juca@email.com' }
 
+// CRUD - Create, Read, Update, Delete
+
 const users = ['Juca', 'Teobaldo', 'Tabajara']
 
 server.get('/users', (req, res) => {
-  // const nome = req.query.nome
-  const { nome } = req.query // utilizando desestruturação
-
-  res.json({ message: `Hello World ${nome}` })
+  return res.json(users)
 })
 
 server.get('/users/:index', (req, res) => {
-  // const id = req.params.id
-  const { index } = req.params // utilizando desestruturação
+  const { index } = req.params
 
-  res.json(users[index])
+  return res.json(users[index])
+})
+
+server.post('/users', (req, res) => {
+  const { nome } = req.body
+
+  users.push(nome)
+
+  return res.json(users)
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params
+  const { nome } = req.body
+
+  users[index] = nome
+
+  return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params
+
+  users.splice(index, 1)
+
+  return res.send()
 })
 
 server.listen(3000)
