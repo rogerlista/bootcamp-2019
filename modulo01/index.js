3fae3eaf9de0f08795182767df988ed57c91a6b5
Rogério <roger.lista@gmail.com>
2019-10-12 11:05:11 -0300

    NodeJS módulo 01 - rotas com query params e route params

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,22 @@ const express = require('express')
 
 const server = express()
 
-server.get('/teste', (req, res) => {
-  res.json({ message: 'Hello World' })
+// query params = /users?nome=Juca
+// route params = /users/42
+// body params = { nome='Juca', email='juca@email.com' }
+
+server.get('/users', (req, res) => {
+  // const nome = req.query.nome
+  const { nome } = req.query // utilizando desestruturação
+
+  res.json({ message: `Hello World ${nome}` })
+})
+
+server.get('/users/:id', (req, res) => {
+  // const id = req.params.id
+  const { id } = req.params // utilizando desestruturação
+
+  res.json({ message: `Buscando o usuário ${id}` })
 })
 
 server.listen(3000)
