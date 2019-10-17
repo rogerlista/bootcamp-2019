f6687c53344955a223b149f2382d358a45c597ac
Rogério <roger.lista@gmail.com>
2019-10-17 07:23:45 -0300

    Desafio 02 - configuração inicial do projeto

---
+++ desafio02/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+import routes from './routes'
+
+class App {
+  constructor() {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.express.use(express.json())
+  }
+
+  routes() {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
