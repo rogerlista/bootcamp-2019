e514ed3c5a9385a84706c12c6a9f70604177015a
Rogério <roger.lista@gmail.com>
2019-10-19 10:23:59 -0300

    NodeJS módulo 02 - upload avatar do perfil do usuário

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import userController from './app/controllers/user-controller'
 import sessionController from './app/controllers/session-controller'
 
 import authMiddleware from './app/middlewares/auth'
+import multerConfig from './config/multer'
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Olá seja bem vindo!' })
@@ -17,5 +20,9 @@ routes.post('/sessions', sessionController.store)
 routes.use(authMiddleware)
 
 routes.put('/users', userController.update)
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true })
+})
 
 export default routes
