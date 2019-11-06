b0057e3a67c8ae45221a705a68fb08692a224f4a
Rogério <roger.lista@gmail.com>
2019-11-06 07:38:06 -0300

    ReactJS módulo04 - ciclo de vida do componente

--- modulo4/src/components/tech-list.js
+++ modulo4/src/components/tech-list.js
@@ -5,8 +5,27 @@ import TechItem from './tech-item'
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: ['Node.js', 'ReactJS', 'React Native']
+    techs: []
   }
+
+  // Executado assim que o componente aparece na tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs')
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) })
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou states
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
 
   handleInputChange = e => {
     console.log(e.target.value)
