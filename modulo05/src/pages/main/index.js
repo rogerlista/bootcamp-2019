0d61688498365bac3df387f81f11023f2cc5e13e
Rogério <roger.lista@gmail.com>
2019-11-15 06:08:22 -0300

    ReactJS módulo05 - estilizando componentes styled components

--- modulo05/src/pages/main/index.js
+++ modulo05/src/pages/main/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import { Title } from './styles'
+
 function Main() {
-  return <h1>Main</h1>
+  return (
+    <Title error>
+      Main
+      <small>menor</small>
+    </Title>
+  )
 }
 
 export default Main
