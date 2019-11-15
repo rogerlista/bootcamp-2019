ed6a0cb2f12cc1294c2dc03067c40f46ee9a1ea0
Rogério <roger.lista@gmail.com>
2019-11-15 06:34:57 -0300

    ReactJS módulo05 - estilizando globalmente os componentes

--- modulo05/src/pages/main/index.js
+++ modulo05/src/pages/main/index.js
@@ -3,12 +3,7 @@ import React from 'react'
 import { Title } from './styles'
 
 function Main() {
-  return (
-    <Title error>
-      Main
-      <small>menor</small>
-    </Title>
-  )
+  return <Title>Hello World</Title>
 }
 
 export default Main
