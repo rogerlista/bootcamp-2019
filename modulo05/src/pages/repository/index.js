30d4e9ab61b2e399be609cc5ad9aa927f89bb320
Rogério <roger.lista@gmail.com>
2019-11-15 19:04:08 -0300

    ReactJS módulo05 - definindo prop types para o component repository

--- modulo05/src/pages/repository/index.js
+++ modulo05/src/pages/repository/index.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import api from '../../services/api'
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  }
+
   state = {
     repository: {},
     issues: [],
@@ -32,6 +41,8 @@ export default class Repository extends Component {
   }
 
   render() {
+    const { repository, issues, loading } = this.state
+
     return <h1>Repository</h1>
   }
 }
