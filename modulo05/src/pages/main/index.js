f8d3602718e51974f07d23577ca8abdf15218a93
Rogério <roger.lista@gmail.com>
2019-11-15 14:00:50 -0300

    ReactJS módulo05 - navegando para rota repository

--- modulo05/src/pages/main/index.js
+++ modulo05/src/pages/main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
 
@@ -84,7 +85,10 @@ class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              {/* encodeURIComponent é uma função do javascript que realiza o encode da barra */}
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
