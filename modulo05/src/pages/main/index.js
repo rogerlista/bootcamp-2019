f4a1b6571c95c83fc013ec83906ac9f4f123fca2
Rogério <roger.lista@gmail.com>
2019-11-15 07:26:11 -0300

    ReactJS módulo05- estilizando a página main para adição de repositórios

--- modulo05/src/pages/main/index.js
+++ modulo05/src/pages/main/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-import { Title } from './styles'
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+
+import { Container, Form, SubmitButton } from './styles'
 
 function Main() {
-  return <Title>Hello World</Title>
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
 }
 
 export default Main
