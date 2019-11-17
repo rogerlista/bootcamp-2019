a1017d59e9f683c0c78d5d7cac8b5455dc92b4c1
Rogério <roger.lista@gmail.com>
2019-11-17 08:54:42 -0300

    React native módulo06 - configurado reactotron

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 
+import './config/reactotron-config'
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -8,7 +10,6 @@ export default function App() {
     </View>
   )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -16,6 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f5fcff',
   },
+
   welcome: {
     fontSize: 20,
     textAlign: 'center',
