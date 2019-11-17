af59108a358ae9b5d475c20c1cf221e798453b89
Rogério <roger.lista@gmail.com>
2019-11-17 17:46:51 -0300

    React native módulo06 - acessando a api do github

--- modulo06/src/pages/main/index.js
+++ modulo06/src/pages/main/index.js
@@ -1,25 +1,60 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import { Keyboard } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import api from '../../services/api'
 
 import { Container, Form, Input, SubmitButton } from './styles'
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  }
 
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+  handleAddUser = async () => {
+    const { users, newUser } = this.state
+
+    const response = await api.get(`/users/${newUser}`)
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    }
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    })
+
+    Keyboard.dismiss()
+  }
+
+  render() {
+    const { newUser } = this.state
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
 }
 
 Main.navigationOptions = {
