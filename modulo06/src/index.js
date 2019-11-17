d374678fbcd7bcc06e353622b3b72d0b9ded8b37
Rogério <roger.lista@gmail.com>
2019-11-17 11:51:09 -0300

    React native módulo06 - configurado o status bar

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 
 import './config/reactotron-config'
 
@@ -7,5 +8,10 @@ import './config/reactotron-config'
 import Routes from './routes'
 
 export default function App() {
-  return <Routes />
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  )
 }
