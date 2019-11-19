66e668dc58014d99b63b55d98621dda40edc3ab7
Rogério <roger.lista@gmail.com>
2019-11-19 07:23:52 -0300

    React native módulo06 - listando favoritos do usuário

--- modulo06/src/pages/user/index.js
+++ modulo06/src/pages/user/index.js
@@ -4,6 +4,20 @@ import { View } from 'react-native'
 
 import api from '../../services/api'
 
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles'
+
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.getParam('user').name,
@@ -29,8 +43,32 @@ export default class User extends Component {
   }
 
   render() {
+    const { navigation } = this.props
     const { stars } = this.state
+    const user = navigation.getParam('user')
 
-    return <View />
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    )
   }
 }
