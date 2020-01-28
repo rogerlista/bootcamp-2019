38e9c07329fca35612e9dbdf4771302b4ea23a5e
Rogério <roger.lista@gmail.com>
2020-01-28 07:14:32 -0300

    CRUD usuário

--- modulo01/query-route-params.js
+++ modulo01/query-route-params.js
@@ -2,21 +2,42 @@ const express = require('express')
 
 const server = express()
 
-const users = ['Juca', 'Teobaldo', 'Tabajara']
-
 // query params -> localhost:3000/users?nome=Juca
 // route params -> localhost:3000/users/34
+// request body -> localhost:3000/users { "name": "Juca", "email", "juca@email.com" }
+
+const users = ['Juca', 'Teobaldo', 'Tabajara'] // repositório
+
+// CRUD - Create, Read, Update, Delete
+
+server.use(express.json())
 
 server.get('/users', (req, res) => {
-  const { nome } = req.query
-
-  return res.json({ message: `Hello ${nome}` })
+  return res.json(users)
 })
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params
-
   return res.json(users[index])
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body
+  users.push(name)
+  return res.json(users)
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+  users[index] = name
+  return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+  const index = req.params
+  users.splice(index, 1)
+  return res.send()
 })
 
 server.listen('3000')
