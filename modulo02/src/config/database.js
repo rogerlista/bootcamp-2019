a5f222a0831f60e00ac262decafe5412b0df12f9
Rogério <roger.lista@gmail.com>
2020-01-29 21:33:49 -0300

    Configurando Sequelize

---
+++ modulo02/src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "postgres",
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
