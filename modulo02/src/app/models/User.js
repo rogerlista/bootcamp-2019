9fe87f254c1c3dd5a4fea551c900a3db722a8b7e
Rogério <roger.lista@gmail.com>
2020-01-31 06:50:47 -0300

    Criado o loader de models

--- modulo02/src/app/models/User.js
+++ modulo02/src/app/models/User.js
@@ -1,4 +1,4 @@
-import Sequelize, { Model } from 'sequelize',
+import Sequelize, { Model } from 'sequelize'
 
 class User extends Model {
   static init(sequelize) {
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -2,8 +2,16 @@ import { Router } from 'express'
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
+import User from './app/models/User'
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Teste teste',
+    email: 'teste@teste.com',
+    password_hash: '3435434345',
+  })
+
+  return res.json(user)
 })
 
 export default routes
