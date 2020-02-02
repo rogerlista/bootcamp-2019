86925de18790b2878d0a324430c046ebbb04b828
Rogério <roger.lista@gmail.com>
2020-02-02 12:04:05 -0300

    Configurado listagem de providers com url do avatar

--- modulo03/src/app.js
+++ modulo03/src/app.js
@@ -1,3 +1,5 @@
+import { resolve } from 'path'
+
 import express from 'express'
 
 import routes from './routes'
@@ -14,6 +16,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json())
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    )
   }
 
   routes() {
