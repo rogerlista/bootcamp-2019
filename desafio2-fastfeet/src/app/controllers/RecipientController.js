6abc78946ded142aada512129d863844247420a1
Rogério <roger.lista@gmail.com>
2020-02-01 21:10:42 -0300

    Configurado model recipient do projeto FastFeet

---
+++ desafio2-fastfeet/src/app/controllers/RecipientController.js
@@ -0,0 +1,88 @@
+import * as Yup from 'yup'
+
+import Recipient from '../models/Recipient'
+
+class RecipientController {
+  async index(req, res) {
+    const recipients = await Recipient.findAll()
+    return res.json(recipients)
+  }
+
+  async show(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id)
+
+    if (!recipient) {
+      return res.status(404).json({ error: 'Recipient does not exists' })
+    }
+
+    return res.json(recipient)
+  }
+
+  async store(req, res) {
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    } = await Recipient.create(req.body)
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    })
+  }
+
+  async update(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id)
+
+    if (!recipient) {
+      return res.status(404).json({ error: 'Recipient does not exists' })
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    } = await recipient.update(req.body)
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    })
+  }
+
+  async delete(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id)
+
+    if (!recipient) {
+      return res.status(404).json({ error: 'Recipient does not exists' })
+    }
+
+    recipient.destroy()
+
+    return res.json()
+  }
+}
+
+export default new RecipientController()
--- desafio2-fastfeet/src/routes.js
+++ desafio2-fastfeet/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import SessionController from './app/controllers/SessionController'
+import RecipientController from './app/controllers/RecipientController'
 
 const routes = new Router()
 
@@ -9,5 +10,11 @@ routes.get('/', (req, res) => {
 })
 
 routes.post('/sessions', SessionController.store)
+
+routes.get('/recipients', RecipientController.index)
+routes.get('/recipients/:id', RecipientController.show)
+routes.post('/recipients', RecipientController.store)
+routes.put('/recipients/:id', RecipientController.update)
+routes.delete('/recipients/:id', RecipientController.delete)
 
 export default routes
