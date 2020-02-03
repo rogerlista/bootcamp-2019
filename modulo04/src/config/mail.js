0dffdd504f998906b4199f635606c44b92c781f8
Rogério <roger.lista@gmail.com>
2020-02-03 06:25:32 -0300

    Configurado Nodemailer

---
+++ modulo04/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c93e61647863ce',
+    pass: 'f235088e6c31ec',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
