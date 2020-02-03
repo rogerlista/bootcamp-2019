1cee3d66589f52f6d25499a20b00abbebdbbb139
Rogério <roger.lista@gmail.com>
2020-02-03 07:27:45 -0300

    Configurado templates de email

--- modulo04/src/app/controllers/AppointmentController.js
+++ modulo04/src/app/controllers/AppointmentController.js
@@ -117,6 +117,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name']
+        }
       ],
     })
 
@@ -141,7 +146,16 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          appointment.date,
+          "'dia' dd 'de' MMMM', às' H:mm'h.'",
+          { locale: pt }
+        )
+      }
     })
 
     return res.json(appointment)
