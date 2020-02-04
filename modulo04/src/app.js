88c94829a90e354c0804ab8e867b21a0938e9e58
Rogério <roger.lista@gmail.com>
2020-02-04 19:29:56 -0300

    Tratamento de exceções

--- modulo04/src/app.js
+++ modulo04/src/app.js
@@ -1,8 +1,13 @@
 import { resolve } from 'path'
 
 import express from 'express'
+import * as Sentry from '@sentry/node'
+import Youch from 'youch'
+
+import 'express-async-errors'
 
 import routes from './routes'
+import sentryConfig from './config/sentry'
 
 import './database'
 
@@ -10,11 +15,14 @@ class App {
   constructor() {
     this.server = express()
 
+    Sentry.init(sentryConfig)
     this.middlewares()
     this.routes()
+    this.exceptionHandler()
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler())
     this.server.use(express.json())
     this.server.use(
       '/files',
@@ -24,6 +32,15 @@ class App {
 
   routes() {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON()
+
+      return res.status(500).json(errors)
+    })
   }
 }
 
