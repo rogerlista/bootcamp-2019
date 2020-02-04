4657363bad5d9fb5cb5aa6bef1e8e0329c278065
Rogério <roger.lista@gmail.com>
2020-02-04 07:11:19 -0300

    Campos virutais no agendamento

--- modulo04/src/app/controllers/AppointmentController.js
+++ modulo04/src/app/controllers/AppointmentController.js
@@ -15,7 +15,7 @@ class AppointmentController {
     const { page = 1 } = req.query
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       order: ['date'],
       limit: 20,
       offset: (page - 1) * 20,
