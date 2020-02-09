ac7b060f7568751d04cf921051e026ccf5b58fc4
Rogério <roger.lista@gmail.com>
2020-02-09 10:31:17 -0300

    Criando o component raiz

--- modulo05-reactjs/src/index.js
+++ modulo05-reactjs/src/index.js
@@ -1,3 +1,6 @@
-const soma = (x, y) => x + y
+import React from 'react'
+import { render } from 'react-dom'
 
-alert(soma(3, 5))
+import App from './App'
+
+render(<App />, document.getElementById('app'))
