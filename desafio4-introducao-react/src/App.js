e789334a55420982fae33125976cc9531d6b9cfa
Rogério <roger.lista@gmail.com>
2020-02-10 21:40:12 -0300

    Criado o componente Header - Desafio 4 Introdução ao React

--- desafio4-introducao-react/src/App.js
+++ desafio4-introducao-react/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import Header from './components/Header'
+
 function App() {
-  return <h1>Olá..</h1>
+  return <Header />
 }
 
 export default App
---
+++ desafio4-introducao-react/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <h1>Facebook</h1>
+      <span>Meu perfil</span>
+      <img src="" />
+    </header>
+  )
+}
+
+export default Header
