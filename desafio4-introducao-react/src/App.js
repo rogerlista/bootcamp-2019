7ac1605c549177cf6cd20fc8a9f491ee1efdc9e1
Rogério <roger.lista@gmail.com>
2020-02-11 12:01:47 -0300

    Configurado CSS estilo - Desafio 4 Introdução ao React

--- desafio4-introducao-react/src/App.js
+++ desafio4-introducao-react/src/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Header from './components/Header'
 import PostList from './components/PostList'
 
+import './App.css'
+
 function App() {
   return (
     <>
--- desafio4-introducao-react/webpack.config.js
+++ desafio4-introducao-react/webpack.config.js
@@ -14,9 +14,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
     ],
   },
