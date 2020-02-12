76e4bcb7a33989cfcc5657432df420656e0c548a
Rogério <roger.lista@gmail.com>
2020-02-11 21:40:24 -0300

    Estilizado componente comment - Desafio 4 Introdução ao React

--- desafio4-introducao-react/src/components/Comment.js
+++ desafio4-introducao-react/src/components/Comment.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import user2 from '../assets/user2.svg'
+
 function Comment() {
   return (
-    <>
-      <img src="" />
+    <div class="comment">
+      <img src={user2} />
       <p>
         <strong>Diego Fernandes</strong> A Rocketseat está sempre em busca de novos membros para o time, e geralmente
         ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do
         Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord!
         (Sério, me chamem mesmo, esse comentário é real)
       </p>
-    </>
+    </div>
   )
 }
 
