a7eed0100de37f16160ba6c2aad8782ca4183868
Rogério <roger.lista@gmail.com>
2020-02-12 06:22:33 -0300

    Corrigido import de CSS global - Desafio 4 Introdução ao React

--- desafio4-introducao-react/src/App.js
+++ desafio4-introducao-react/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
+import './App.css'
+
 import Header from './components/Header'
 import PostList from './components/PostList'
-
-import './App.css'
 
 function App() {
   return (
