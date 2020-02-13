77efce85617668b77126858a2ce23131d3036f02
Rogério <roger.lista@gmail.com>
2020-02-13 07:18:01 -0300

    Styled components - ReactJS

--- modulo06-primeiro-projeto-reactjs/src/pages/Main/index.js
+++ modulo06-primeiro-projeto-reactjs/src/pages/Main/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import { Title } from './styles'
+
 export default function Main() {
-  return <h1>Main</h1>
+  return (
+    <Title error={false}>
+      Main
+      <small> menor</small>
+    </Title>
+  )
 }
