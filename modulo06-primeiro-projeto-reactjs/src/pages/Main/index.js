e4e94d76560e90f9f5ea33f6e85f70289df425ec
Rogério <roger.lista@gmail.com>
2020-02-14 06:39:08 -0300

    Estilizando página Main repositórios github - ReactJS

--- modulo06-primeiro-projeto-reactjs/src/pages/Main/index.js
+++ modulo06-primeiro-projeto-reactjs/src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
 
-import { Title } from './styles'
+import { Container, Form, SubmitButton } from './styles'
 
 export default function Main() {
-  return <Title>Hello World</Title>
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositório
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
 }
