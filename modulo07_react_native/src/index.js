a6bf578ba1d7e132fae2ec35f5fd166da12ef9a1
Rogério <roger.lista@gmail.com>
2020-02-22 13:05:38 -0300

    Configurado StatusBar - React Native

--- modulo07_react_native/src/index.js
+++ modulo07_react_native/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 
@@ -9,6 +10,7 @@ import Routes from './routes'
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
     </NavigationContainer>
   )
