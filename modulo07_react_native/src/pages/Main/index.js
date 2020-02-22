6a6c9f6745b2c13704cf0b47c49f03314fb5b32c
Rogério <roger.lista@gmail.com>
2020-02-22 13:20:41 -0300

    Configurado Styled Components - React Native

--- modulo07_react_native/src/pages/Main/index.js
+++ modulo07_react_native/src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View } from 'react-native'
+
+import { Container } from './styles'
 
 export default function Main() {
-  return <View />
+  return <Container />
 }
