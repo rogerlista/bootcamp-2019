54ce737b14654d55f2242b7517aeeb9d4eba2b09
Rogério <roger.lista@gmail.com>
2020-02-23 07:16:33 -0300

    Estilizando formulário com botões e icones - React Native

--- modulo07_react_native/src/pages/Main/index.js
+++ modulo07_react_native/src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import { Container } from './styles'
+import { Container, Form, Input, SubmitButton } from './styles'
 
 export default function Main() {
-  return <Container />
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
 }
