c9ede1fd006c559df65e2583622d34b026b94bbf
Rogério <roger.lista@gmail.com>
2020-02-23 21:28:23 -0300

    Estilizando listagem - React Native

--- modulo07_react_native/src/pages/Main/index.js
+++ modulo07_react_native/src/pages/Main/index.js
@@ -4,7 +4,19 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import api from '../../services/api'
 
-import { Container, Form, Input, SubmitButton } from './styles'
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles'
 
 export default class Main extends Component {
   state = {
@@ -52,6 +64,22 @@ export default class Main extends Component {
             <Icon name="add" size={20} color="#fff" />
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     )
   }
