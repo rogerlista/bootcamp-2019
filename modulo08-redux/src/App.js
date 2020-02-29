012631d631a05503d043d3e8985353693823b546
Rogério <roger.lista@gmail.com>
2020-02-29 11:52:20 -0300

    Módulo 8 - configurado o redux - Redux

--- modulo08-redux/src/App.js
+++ modulo08-redux/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import GlobalStyle from './styles/global'
 import Header from './components/Header'
 import Routes from './routes'
+import store from './store'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   )
 }
