f15f27f9be1a83208eb50c03f96bca44fba84b3e
Rogério <roger.lista@gmail.com>
2020-02-29 13:58:12 -0300

    Módulo 8 - configurado reactotron com redux - Redux

--- modulo08-redux/src/App.js
+++ modulo08-redux/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
+import './config/reactotron'
+
 import GlobalStyle from './styles/global'
 import Header from './components/Header'
 import Routes from './routes'
