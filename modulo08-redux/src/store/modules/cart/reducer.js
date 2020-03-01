8fc74807cf264ffbe24097546f4b8ac66109e23d
Rogério <roger.lista@gmail.com>
2020-03-01 06:34:31 -0300

    Módulo 8 - remover produto - Redux

--- modulo08-redux/src/store/modules/cart/reducer.js
+++ modulo08-redux/src/store/modules/cart/reducer.js
@@ -17,6 +17,16 @@ export default function cart(state = [], action) {
           })
         }
       })
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          product => product.id === action.id
+        )
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1)
+        }
+      })
     default:
       return state
   }
