4ad92f3bd7dbbbf4b0cbca72f95264e711bcaa3e
Rogério <roger.lista@gmail.com>
2020-03-03 07:17:10 -0300

    Módulo 8 - configurado react toastify mensagens - Redux

--- modulo08-redux/src/App.js
+++ modulo08-redux/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 
 import './config/reactotron'
 
@@ -16,6 +17,7 @@ export default function App() {
         <GlobalStyle />
         <Header />
         <Routes />
+        <ToastContainer />
       </BrowserRouter>
     </Provider>
   )
