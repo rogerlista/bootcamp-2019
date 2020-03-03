68407862ff98c0cd0ebf9db8f637baa66c66bbef
Rogério <roger.lista@gmail.com>
2020-03-03 13:29:08 -0300

    Módulo 8 - navegacao no saga - Redux

--- modulo08-redux/src/App.js
+++ modulo08-redux/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 
@@ -8,17 +8,18 @@ import './config/reactotron'
 import GlobalStyle from './styles/global'
 import Header from './components/Header'
 import Routes from './routes'
+import history from './services/history'
 import store from './store'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <GlobalStyle />
         <Header />
         <Routes />
         <ToastContainer />
-      </BrowserRouter>
+      </Router>
     </Provider>
   )
 }
