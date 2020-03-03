6542bd22cb36024bc63483a341329a671a58fd80
Rogério <roger.lista@gmail.com>
2020-03-03 12:48:48 -0300

    Módulo 8 - validando estoque na alteração da quantidade - Redux

--- modulo08-redux/src/pages/Cart/index.js
+++ modulo08-redux/src/pages/Cart/index.js
@@ -14,13 +14,13 @@ import { formatPrice } from '../../util/format'
 
 import { Container, ProductTable, Total } from './styles'
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1)
+    updateAmountRequest(product.id, product.amount - 1)
   }
 
   function increment(product) {
-    updateAmount(product.id, product.amount + 1)
+    updateAmountRequest(product.id, product.amount + 1)
   }
 
   return (
