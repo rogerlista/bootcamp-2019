329a7e9cae46b93a2bb5dfaf17a1677b9e150dc8
Rogério <roger.lista@gmail.com>
2020-03-03 21:41:45 -0300

    Desafio 7 - configurado eslint priettier e editorconfig - React Native

--- desafio7_e_commerce_react_native/.eslintrc.js
+++ desafio7_e_commerce_react_native/.eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: { 'prettier/prettier': 'error' },
+}
