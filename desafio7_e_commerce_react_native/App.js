a48acd851e2376636565d16bc22ad195b8eeb46c
Rogério <roger.lista@gmail.com>
2020-03-03 21:07:49 -0300

    Desafio 7 - estrutura inicial do projeto - React Native

---
+++ desafio7_e_commerce_react_native/App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to E-Commerce</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#191920',
+  },
+  welcome: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
