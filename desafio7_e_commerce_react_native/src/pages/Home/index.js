ca157e1bf75bef751f26c004e5a0fcb82f2ef79d
Rogério <roger.lista@gmail.com>
2020-03-07 10:05:55 -0300

    Desafio 7 - estilizando página home - React Native

--- desafio7_e_commerce_react_native/src/pages/Home/index.js
+++ desafio7_e_commerce_react_native/src/pages/Home/index.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import { Container } from './styles'
+import { formatPrice } from '../../util/format'
+import api from '../../services/api'
 
-export default function Home() {
-  return <Container />
+import {
+  Container,
+  ProductList,
+  Product,
+  Imagem,
+  Description,
+  Price,
+  AddButton,
+  IconContainer,
+  IconButton,
+  IconButtonText,
+  AddButtonText,
+} from './styles'
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products')
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+
+    this.setState({ products: data })
+  }
+
+  render() {
+    const { products } = this.state
+
+    return (
+      <Container>
+        <ProductList
+          data={products}
+          keyExtractor={product => String(product.id)}
+          renderItem={({ item }) => (
+            <Product>
+              <Imagem source={{ uri: item.image }} />
+              <Description>{item.title}</Description>
+              <Price>{item.priceFormatted}</Price>
+
+              <AddButton onPress={() => {}}>
+                <IconContainer>
+                  <IconButton name="add-shopping-cart" />
+                  <IconButtonText>4</IconButtonText>
+                </IconContainer>
+                <AddButtonText>Adicionar</AddButtonText>
+              </AddButton>
+            </Product>
+          )}
+        />
+      </Container>
+    )
+  }
 }
