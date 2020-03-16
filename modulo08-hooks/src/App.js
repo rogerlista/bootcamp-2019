d498ed7cfcbdda348ebc6911adb234cf1709e1fd
Rogério <roger.lista@gmail.com>
2020-03-16 07:16:43 -0300

    Hooks - useState

--- modulo08-hooks/src/App.js
+++ modulo08-hooks/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return <h1>Hello world</h1>
+  const [techs, setTechs] = useState(['ReactJS', 'React Native'])
+  const [newTech, setNewTech] = useState('')
+
+  function handleAdd() {
+    setTechs([...techs, newTech])
+    setNewTech('')
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+
+      <input
+        value={newTech}
+        onChange={event => setNewTech(event.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  )
 }
 
 export default App
