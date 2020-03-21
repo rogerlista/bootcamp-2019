c737169bae559a6583353b0647255d0410604b33
Rogério <roger.lista@gmail.com>
2020-03-21 07:31:46 -0300

    Módulo 9 - validando os dados dos inputs

--- modulo09-web/src/pages/SignUp/index.js
+++ modulo09-web/src/pages/SignUp/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import * as Yup from 'yup'
+
 import { Form, Input } from '@rocketseat/unform'
 
 import logo from '~/assets/logo.svg'
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+})
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -14,7 +26,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Seu nome completo" />
         <Input name="email" type="email" placeholder="Seu e-email" />
         <Input
