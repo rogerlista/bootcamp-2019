e416b6f8f84c2d7b5cd601d96decd2bdba474085
Rogério <roger.lista@gmail.com>
2020-03-23 06:13:04 -0300

    Módulo 9 - loading da autenticação

--- modulo09-web/src/pages/SignIn/index.js
+++ modulo09-web/src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import * as Yup from 'yup'
@@ -19,6 +19,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch()
+  const loading = useSelector(state => state.auth.loading)
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password))
@@ -35,7 +36,7 @@ export default function SignIn() {
           placeholder="Sua senha secreta"
         />
 
-        <button type="submit">Acessar</button>
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
         <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
