f517078f32761d9b835195b398e7a3a2670e0f85
Rogério <roger.lista@gmail.com>
2020-03-24 22:09:55 -0300

    Módulo 9 - estilizando notificações

--- modulo09-web/src/styles/global.js
+++ modulo09-web/src/styles/global.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
+import 'react-perfect-scrollbar/dist/css/styles.css'
+
 import 'react-toastify/dist/ReactToastify.css'
 
 export default createGlobalStyle`
