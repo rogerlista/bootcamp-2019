3e952f78d4c8539fe3d0a4669949d46f7e3840f4
Rogério <roger.lista@gmail.com>
2020-03-25 21:20:04 -0300

    Módulo 9 - pagina perfil

--- modulo09-web/src/pages/Profile/index.js
+++ modulo09-web/src/pages/Profile/index.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-// import { Container } from './styles'
+import { Form, Input } from '@rocketseat/unform'
+
+import { Container } from './styles'
 
 export default function Profile() {
-  return <h1>Profile</h1>
+  const profile = useSelector(state => state.user.profile)
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme a senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  )
 }
