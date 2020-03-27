7f901cecba46a4f070f1df0758ca0740c18bcc57
Rogério <roger.lista@gmail.com>
2020-03-27 06:22:47 -0300

    Módulo 9 - dados header

--- modulo09-web/src/components/Header/index.js
+++ modulo09-web/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import Notifications from '~/components/Notifications'
@@ -8,6 +9,8 @@ import logo from '~/assets/logo-purple.svg'
 import { Container, Content, Profile } from './styles'
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile)
+
   return (
     <Container>
       <Content>
@@ -21,13 +24,16 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Juca Bala</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
 
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Juca Bala"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
