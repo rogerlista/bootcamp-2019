fc632283519ff70cc559827a1c7209912fae9311
Rogério <roger.lista@gmail.com>
2020-03-28 06:11:20 -0300

    Módulo 9 - logout da aplicação

--- modulo09-web/src/pages/Profile/index.js
+++ modulo09-web/src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { Form, Input } from '@rocketseat/unform'
 
+import { signOut } from '~/store/modules/auth/actions'
 import { updateProfileRequest } from '~/store/modules/user/actions'
 
 import AvatarInput from './AvatarInput'
@@ -15,6 +16,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data))
+  }
+
+  function handleSignOut() {
+    dispatch(signOut())
   }
 
   return (
@@ -42,7 +47,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   )
 }
