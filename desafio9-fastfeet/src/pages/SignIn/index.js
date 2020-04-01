8ac8abd3cd3bbdb13f3f56be0f51a53d6dd6b021
Rogério <roger.lista@gmail.com>
2020-04-01 05:57:15 -0300

    Desafio 9 - utilizando unform na tela de login

--- desafio9-fastfeet/src/pages/SignIn/index.js
+++ desafio9-fastfeet/src/pages/SignIn/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
+import { Form, Input } from '@rocketseat/unform'
+
 import logo from '~/assets/fastfeet-logo.png'
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data)
+  }
+
   return (
     <>
       <img src={logo} alt="Logo FastFeet" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <label htmlFor="email">Seu e-mail</label>
-        <input type="email" placeholder="examplo@email.com" />
+        <Input name="email" type="email" placeholder="examplo@email.com" />
         <label htmlFor="password">Sua senha</label>
-        <input type="password" placeholder="************" />
+        <Input name="password" type="password" placeholder="************" />
 
         <button type="submit" className="button">
           Entrar no sistema
         </button>
-      </form>
+      </Form>
     </>
   )
 }
