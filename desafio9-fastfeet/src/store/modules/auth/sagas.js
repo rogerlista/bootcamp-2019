2f284c46badfc0bb4e428fa5a704c8010d56658b
Rogério <roger.lista@gmail.com>
2020-04-02 07:09:19 -0300

    Desafio 9 - configurado o loading da autenticação

--- desafio9-fastfeet/src/store/modules/auth/sagas.js
+++ desafio9-fastfeet/src/store/modules/auth/sagas.js
@@ -3,7 +3,7 @@ import { takeLatest, call, put, all } from 'redux-saga/effects'
 import history from '~/services/history'
 import api from '~/services/api'
 
-import { signInSuccess } from './actions'
+import { signInSuccess, signInFailure } from './actions'
 
 export function* signIn({ payload }) {
   const { email, password } = payload
@@ -20,7 +20,7 @@ export function* signIn({ payload }) {
 
     history.push('/order-list')
   } catch (err) {
-    console.tron.error(err)
+    yield put(signInFailure())
   }
 }
 
